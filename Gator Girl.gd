5f3a7d45e4b10f36fee652f31fd11efb9b0f4aa1
dmglynn <dmglynn@ncsu.edu>
2021-09-04 13:39:01 -0400

    added basic movement + tileset

--- Gator Girl.gd
+++ Gator Girl.gd
@@ -1,17 +1,21 @@
 extends KinematicBody2D
 
 
+var velocity = Vector2.ZERO
+var speed = 600
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 func _physics_process(delta):
-	position += get_direction();
+	velocity = get_direction() * speed
+	velocity = move_and_slide(velocity)
 
 
 func get_direction(): #get direction of the character 
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	)
+	).normalized()
