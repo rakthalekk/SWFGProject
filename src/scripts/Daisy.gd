1f2a2e37090ef9c3d47a81e6b0b4af1938cce9d9
dmglynn <dmglynn@ncsu.edu>
2021-09-06 00:57:31 -0400

    balls

--- src/scripts/Daisy.gd
+++ src/scripts/Daisy.gd
@@ -3,8 +3,8 @@ extends "res://src/scripts/Enemy.gd"
 
 func _init():
 	contact_dmg = 20
-	walk_speed = 800
-	hp = 40
+	walk_speed = 1200
+	hp = 50
 	direction = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized()
 
 
@@ -19,7 +19,6 @@ func handle_behavior():
 		var collision = get_slide_collision(i)
 		var collider = collision.collider
 		if collider.is_in_group("wall"):
-			print(collision.normal)
 			direction = direction.bounce(collision.normal)
 	velocity = direction * walk_speed
 
--- src/scripts/WordAttack.gd
+++ src/scripts/WordAttack.gd
@@ -1,12 +1,41 @@
 class_name WordAttack
 extends Area2D
 
+const BALLS = preload("res://assets/Projectiles/balls.png")
+const BESTIE = preload("res://assets/Projectiles/bestie.png")
+const BRUH = preload("res://assets/Projectiles/bruh.png")
+const CRANKING = preload("res://assets/Projectiles/cranking.png")
+const DESERT = preload("res://assets/Projectiles/desert.png")
+const LMAO = preload("res://assets/Projectiles/lmao.png")
+const SOTRUE = preload("res://assets/Projectiles/sotrue.png")
+
 var direction = Vector2.ZERO
 var speed = 15
 var lifespan = 300
 var dmg = 5
 
+onready var sprite = $Sprite
+onready var anim_player = $AnimationPlayer
+
+func _ready():
+	match(randi()%7+1):
+		1:
+			sprite.texture = BALLS
+		2:
+			sprite.texture = BESTIE
+		3:
+			sprite.texture = BRUH
+		4:
+			sprite.texture = CRANKING
+		5:
+			sprite.texture = DESERT
+		6:
+			sprite.texture = LMAO
+		7:
+			sprite.texture = SOTRUE
+
 func _physics_process(delta):
+	anim_player.play("word_attack")
 	if (lifespan <= 0):
 		queue_free()
 	position += direction * speed
@@ -17,3 +46,4 @@ func _on_WordAttack_body_entered(body):
 	if body is GatorGirl:
 		body.damage(dmg)
 		queue_free()
+
